1dcca6bb6f69d922669d1d06602845e2468ff79c
cconway1-stevens <55499408+cconway1-stevens@users.noreply.github.com>
2022-09-30 21:51:53 -0400

    Hi Leena

--- HW_2a/Triangle.py
+++ HW_2a/Triangle.py
@@ -1,26 +1,5 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Jan 14 13:44:00 2016
-Updated Jan 21, 2018
-The primary goal of this file is to demonstrate a simple python program to classify
-triangles
-@author: jrr
-@author: rk
-"""
 def classifyTriangle(a,b,c):
-    """
-    Your correct code goes here...  Fix the faulty logic below until the code passes all ofyou test cases. 
-    
-    This function returns a string with the type of triangle from three integer values corresponding to the lengths of the three sides of the Triangle.
-    
-    return:
-        If all three sides are equal, return 'Equilateral'
-        If exactly one pair of sides are equal, return 'Isoceles'
-        If no pair of  sides are equal, return 'Scalene'
-        If not a valid triangle, then return 'NotATriangle'
-        If the sum of any two sides equals the squate of the third side, then return 'Right'
-        BEWARE: there may be a bug or two in this code
-    """
+  
     # require that the input values be >= 0 and <= 200
     if a > 200 or b > 200 or c > 200:
         return 'InvalidInput'
@@ -31,7 +10,7 @@ def classifyTriangle(a,b,c):
     # verify that all 3 inputs are integers  
     # Python's "isinstance(object,type) returns True if the object is of the specified type
     if not(isinstance(a,int) and isinstance(b,int) and isinstance(c,int)):
-        return 'InvalidInput';
+        return 'InvalidInput'
       
     # This information was not in the requirements spec but 
     # is important for correctness
@@ -41,11 +20,12 @@ def classifyTriangle(a,b,c):
         return 'NotATriangle'
         
     # now we know that we have a valid triangle 
-    if a == b and b == a and a==c:
+    if a == b and b == a:
         return 'Equilateral'
     elif ((a ** 2) + (b ** 2)) == (c ** 2) or ((b** 2) + (c ** 2)) == (a ** 2) or((c ** 2) + (b ** 2)) == (b ** 2) :
         return 'Right'
-    elif (a != b) and  (b != c) and (a != c):
+    elif (a != b) and  (b != c) and (a != b):
         return 'Scalene'
     else:
         return 'Isoceles'
+
